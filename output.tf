2011cb7fdbbcfdbaca085b7ce742d7e4e189aed0
nitin-das <nitin.das@thomson.co.uk>
2020-03-16 13:20:29 +0000

    Modified Code

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "id" {
-  value = aws_s3_bucket.storage_s3.id
+  value = aws_s3_bucket.s3_bucket.id
 }
 
 output "arn" {
-  value = aws_s3_bucket.storage_s3.arn
+  value = aws_s3_bucket.s3_bucket.arn
 }
 
